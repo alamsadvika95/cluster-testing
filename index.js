f4487ec4f9d7b4d953d023f7bba46899c0de5779
Sad Vika Alam <sad.vika04@mastersystem.co.id>
2022-10-03 15:09:09 +0700

    add route

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Hello Test');
+});
+
 //Configuration for Port 
 app.listen(process.env.PORT, () => {
   console.log( `Backend server is running on port ${process.env.PORT}`);
