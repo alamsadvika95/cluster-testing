63572c8b6526597051cd87105e46313e2aaedc58
Sad Vika Alam <sad.vika04@mastersystem.co.id>
2022-10-03 14:05:16 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+
+//Configuration for .env
+dotenv.config();
+
+//Routes
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+//Configuration for Port 
+app.listen(process.env.PORT, () => {
+  console.log( `Backend server is running on port ${process.env.PORT}`);
+});
